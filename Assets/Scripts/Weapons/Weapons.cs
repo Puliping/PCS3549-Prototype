b2887ee91e5a570c56b312955f63b3bcac2ffb36
Gamonga <gabrielprodrigues@usp.br>
2022-09-06 20:14:00 -0300

    Created base for the game

---
+++ Assets/Scripts/Weapons/Weapons.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    public float damage;
+    public enum WeaponType
+    {
+        Sword,
+        Staff,
+        Book,       
+        Bow
+    }
+    public WeaponType type;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
