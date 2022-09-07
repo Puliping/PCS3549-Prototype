041267eb0714d95b8395d2a0fcaa734107c808b1
Paulo Rubens <paulo.rubens@usp.br>
2022-09-07 19:13:08 -0300

    projectile shooting

--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -8,21 +8,35 @@ public class Player : MonoBehaviour
     public float hp;
     public float movSpeed;
     public GameObject arrowPrefab;
+    Vector2 aimDirection;
+    bool firing;
 
-    void OnFire(InputValue value)
+    void OnAim(InputValue value)
     {
-        Vector2 mousePosition = Mouse.current.position.ReadValue();
-        StartCoroutine(fireAt(mousePosition));
+        Vector2 dir = value.Get<Vector2>();
+        if (PlayerInput.GetPlayerByIndex(0).currentControlScheme == "Keyboard and Mouse")
+        {
+            dir.x -= Screen.width / 2;
+            dir.y -= Screen.height / 2;
+            dir.Normalize();
+        }
+        aimDirection = new Vector2(dir.x, dir.y);
+    }
+    void OnFire()
+    {
+        if (!firing)
+        {
+            StartCoroutine(FireAt(aimDirection));
+        }
     }
 
-    IEnumerator fireAt(Vector2 mousePosition)
+    IEnumerator FireAt(Vector2 aimDirection)
     {
-        Vector3 worldPos;
-
+        firing = true;
         GameObject projectile = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
-        worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, 0, mousePosition.y));
-        worldPos.Normalize();
-        projectile.GetComponent<Rigidbody2D>().velocity = worldPos;
-        yield return new WaitForSeconds(1);
+        projectile.GetComponent<Rigidbody2D>().velocity = aimDirection;
+        projectile.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg);
+        yield return new WaitForSeconds(0f);
+        firing = false;
     }
 }
