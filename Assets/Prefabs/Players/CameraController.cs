e01fd46c20c2648e098d92557467319f5a354043
Lucas de Menezes <lucasmenezes098@gmail.com>
2022-09-07 17:56:43 -0300

    camera follows player; initial arrow shooting

---
+++ Assets/Prefabs/Players/CameraController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Players/Player.cs
+++ Assets/Scripts/Players/Player.cs
@@ -1,20 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float hp;
     public float movSpeed;
-    // Start is called before the first frame update
-    void Start()
+    public GameObject arrowPrefab;
+
+    void OnFire(InputValue value)
     {
-        
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        StartCoroutine(fireAt(mousePosition));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator fireAt(Vector2 mousePosition)
     {
-        
+        Vector3 worldPos;
+
+        GameObject projectile = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
+        worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, 0, mousePosition.y));
+        worldPos.Normalize();
+        projectile.GetComponent<Rigidbody2D>().velocity = worldPos;
+        yield return new WaitForSeconds(1);
     }
 }
